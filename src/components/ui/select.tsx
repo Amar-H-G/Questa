90125aff5a62ac876570d9f9c3494759aed8a99d
Amar Patra <amarpatra932@gmail.com>
2025-06-25 01:04:03 +0530

    final  location issue

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import * as SelectPrimitive from "@radix-ui/react-select";
 import { Check, ChevronDown, ChevronUp } from "lucide-react";
 // Make sure 'cn' is exported from '@/lib/utils', or define it here if missing
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
 // If '@/lib/utils' does not export 'cn', you can define a simple 'cn' utility as follows:
 // const cn = (...classes: (string | undefined | false)[]) => classes.filter(Boolean).join(' ');
 
